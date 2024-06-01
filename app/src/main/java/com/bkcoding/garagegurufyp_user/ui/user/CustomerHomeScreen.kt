0926e79e9f6db76e26bb304a46f28d5259cbfdc6
Muhammad Waqas <waqaswaseem679@gmail.com>
2024-06-01 16:11:22 +0500

    ge/show all garage list

--- app/src/main/java/com/bkcoding/garagegurufyp_user/ui/user/CustomerHomeScreen.kt
+++ app/src/main/java/com/bkcoding/garagegurufyp_user/ui/user/CustomerHomeScreen.kt
@@ -2,27 +2,27 @@ package com.bkcoding.garagegurufyp_user.ui.user
 
 import android.Manifest
 import android.os.Build
+import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.rememberScrollState
@@ -31,7 +31,11 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -40,6 +44,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -49,8 +54,11 @@ import androidx.compose.ui.unit.sp
 import androidx.core.content.ContextCompat
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import coil.compose.AsyncImage
 import com.bkcoding.garagegurufyp_user.R
+import com.bkcoding.garagegurufyp_user.dto.Garage
 import com.bkcoding.garagegurufyp_user.extensions.showToast
+import com.bkcoding.garagegurufyp_user.repository.Result
 import com.bkcoding.garagegurufyp_user.ui.AuthViewModel
 import com.bkcoding.garagegurufyp_user.ui.UserViewModel
 import com.bkcoding.garagegurufyp_user.ui.login.UserStorageVM
@@ -68,8 +76,13 @@ fun CustomerHomeScreen(
     onLogOut: () -> Unit
 ){
     val context = LocalContext.current
+    var garageList by rememberSaveable {
+        mutableStateOf<List<Garage>?>(null)
+    }
+
     LaunchedEffect(key1 = Unit) {
         userViewModel.refreshFcmToken()
+        userViewModel.getGarages()
     }
     val permissionLauncher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.RequestPermission()
@@ -92,11 +105,25 @@ fun CustomerHomeScreen(
         }
     }
 
-    CustomerHomeScreen()
+    when (val homeUiState = userViewModel.homeScreenUIState) {
+        Result.Loading -> {
+            //isLoading = true
+            Log.i("TAG", "GarageScreen: loading")
+        }
+
+        is Result.Success -> {
+            garageList = homeUiState.data
+        }
+
+        is Result.Failure -> {}
+        else -> {}
+    }
+
+    CustomerHomeScreen(garageList = garageList)
 }
 
 @Composable
-private fun CustomerHomeScreen() {
+private fun CustomerHomeScreen(garageList: List<Garage>?) {
     Column(
         modifier = Modifier
             .padding(horizontal = 10.dp)
@@ -125,7 +152,37 @@ private fun CustomerHomeScreen() {
             modifier = Modifier.fillMaxWidth()
         )
         Spacer(modifier = Modifier.height(20.dp))
-        ItemList()
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween){
+            Text(
+                text = stringResource(id = R.string.garage),
+                fontSize = 18.sp,
+                fontFamily = FontFamily.Serif,
+                fontWeight = FontWeight.ExtraBold,
+                textAlign = TextAlign.Start,
+                lineHeight = 15.sp,
+                color = Color.Black
+            )
+            Text(
+                text = stringResource(id = R.string.view_all),
+                fontSize = 18.sp,
+                fontFamily = FontFamily.Serif,
+                fontWeight = FontWeight.ExtraBold,
+                textAlign = TextAlign.End,
+                lineHeight = 15.sp,
+                color = Color.Black
+            )
+        }
+        LazyRow(
+            modifier = Modifier
+                .fillMaxWidth()
+                .heightIn(max = 800.dp),
+            contentPadding = PaddingValues(bottom = 20.dp),
+            horizontalArrangement = Arrangement.spacedBy(15.dp)
+        ) {
+            items(garageList.orEmpty()) { item ->
+                GarageCard(modifier = Modifier, item)
+            }
+        }
     }
 }
 
@@ -142,8 +199,7 @@ fun AutoSwipeViewPager(modifier: Modifier = Modifier) {
             .height(220.dp)
             .padding(horizontal = 8.dp)
             .background(
-                color = colorResource(id = R.color.orange50),
-                shape = RoundedCornerShape(10.dp)
+                color = colorResource(id = R.color.orange50), shape = RoundedCornerShape(10.dp)
             )
     ) {
         HorizontalPager(
@@ -173,52 +229,58 @@ fun AutoSwipeViewPager(modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun ItemList() {
-    val titleList = listOf("Request", "View Garages", "Bids", "Chat")
-
-    LazyVerticalGrid(
-        modifier = Modifier.heightIn(max = 800.dp),
-        columns = GridCells.Fixed(2),
-        contentPadding = PaddingValues(12.dp)
-    ) {
-        items(titleList) {
-            ItemDesign(title = it)
-        }
-    }
-}
-
-@Composable
-fun ItemDesign(modifier: Modifier = Modifier, title: String) {
+private fun GarageCard(modifier: Modifier = Modifier, garage: Garage) {
     Box(
-        modifier = modifier
-            .size(130.dp)
-            .clickable { }
-            .padding(5.dp)
-            .border(
-                width = 1.dp,
-                color = Color.Black,
-                shape = RoundedCornerShape(10.dp)
-            )
+        modifier = modifier.background(color = Color.LightGray, shape = RoundedCornerShape(12.dp))
     ) {
-        Box(modifier = Modifier.matchParentSize()) {
-            Text(
-                text = title,
-                fontSize = 20.sp,
-                color = colorResource(id = R.color.black),
-                textAlign = TextAlign.Center,
-                fontWeight = FontWeight.Bold,
+        Column(modifier = Modifier.padding(horizontal = 5.dp, vertical = 5.dp)) {
+            AsyncImage(
+                model = garage.images.getOrNull(0),
+                contentDescription = "",
+                placeholder = painterResource(id = R.drawable.ic_launcher_background),
+                error = painterResource(id = R.drawable.ic_launcher_background),
+                contentScale = ContentScale.Crop,
                 modifier = Modifier
-                    .align(Alignment.Center)
-                    .padding(5.dp)
+                    .padding(0.dp)
+                    .width(150.dp)
+                    .height(100.dp)
+                    .clip(shape = RoundedCornerShape(12.dp))
+            )
+            Text(
+                text = garage.name,
+                fontSize = 15.sp,
+                fontFamily = FontFamily.Serif,
+                fontWeight = FontWeight.SemiBold,
+                textAlign = TextAlign.Start,
+                color = Color.Black,
+                modifier = Modifier.padding(top = 10.dp)
+            )
+            Text(
+                text = garage.location,
+                fontSize = 12.sp,
+                fontFamily = FontFamily.Serif,
+                fontWeight = FontWeight.Normal,
+                textAlign = TextAlign.Start,
+                color = Color.Black,
+                modifier = Modifier.padding(top = 5.dp)
+            )
+            Text(
+                text = garage.phoneNumber,
+                fontSize = 12.sp,
+                fontFamily = FontFamily.Serif,
+                fontWeight = FontWeight.Normal,
+                textAlign = TextAlign.Start,
+                color = Color.Black,
+                modifier = Modifier.padding(top = 5.dp, bottom = 5.dp)
             )
         }
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun CustomerHomeScreenPreview() {
     GarageGuruFypUserTheme {
-        CustomerHomeScreen()
+        CustomerHomeScreen(garageList = emptyList())
     }
 }
