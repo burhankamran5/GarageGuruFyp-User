d425e6386b35e2564e068734b0958c7cfb112613
Muhammad Waqas <waqaswaseem679@gmail.com>
2024-06-10 23:07:48 +0500

    Revert "fix notification foreground issue"
    This reverts commit 92b31a04700e177f6d2dc6c5e07325746b5c3105.

--- app/src/main/java/com/bkcoding/garagegurufyp_user/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/bkcoding/garagegurufyp_user/MyFirebaseMessagingService.kt
@@ -1,14 +1,7 @@
 package com.bkcoding.garagegurufyp_user
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.content.Context
-import android.media.RingtoneManager
-import android.os.Build
-import androidx.core.app.NotificationCompat
 import com.bkcoding.garagegurufyp_user.repository.fcm.FcmRepository
 import com.google.firebase.messaging.FirebaseMessagingService
-import com.google.firebase.messaging.RemoteMessage
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -16,45 +9,8 @@ import javax.inject.Inject
 class MyFirebaseMessagingService:  FirebaseMessagingService() {
     @Inject
     lateinit var fcmRepository: FcmRepository
-    private val notificationID = 123
-
     override fun onNewToken(token: String) {
         super.onNewToken(token)
         fcmRepository.updateFcmToken(token)
-    }
-
-    override fun onMessageReceived(message: RemoteMessage) {
-        super.onMessageReceived(message)
-        if (message.notification != null) {
-            val title = message.notification?.title
-            val body = message.notification?.body
-            showNotification(title, body)
-        }
-    }
-
-    private fun showNotification(title: String?, body: String?) {
-        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        val channelId = "GarageChannelId"
-        val channelName = "GarageChannelName"
-        val notificationSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
-        val notificationBuilder = NotificationCompat.Builder(this, channelId)
-            .setContentTitle(title)
-            .setContentText(body)
-            .setSmallIcon(R.drawable.mechanic_icon_com)
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-            .setSound(notificationSoundUri)
-            .setVibrate(LongArray(1) { 500 }) // Optional vibration
-            .setAutoCancel(true)
-
-        // You can customize the notification further based on your requirements
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channel = NotificationChannel(
-                channelId,
-                channelName,
-                NotificationManager.IMPORTANCE_DEFAULT
-            )
-            notificationManager.createNotificationChannel(channel)
-        }
-        notificationManager.notify(notificationID, notificationBuilder.build())
     }
 }
